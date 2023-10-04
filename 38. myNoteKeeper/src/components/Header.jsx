c928eddf8b1d4b33f7a286e04acf174a5dd02bf1
Merve Çalık <57195581+mivCalik@users.noreply.github.com>
2023-10-05 00:24:11 +0300

    Add files via upload

---
+++ 38. myNoteKeeper/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
+
+function Header() {
+  return (
+    <header>
+      <h1><TipsAndUpdatesIcon/> Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
